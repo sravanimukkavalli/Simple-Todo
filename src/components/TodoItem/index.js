0f6e126bff2f1443adcd999838fe4d0023bb4b13
sravanimukkavalli <111575931+sravanimukkavalli@users.noreply.github.com>
2024-05-10 12:39:46 +0530

    Added Edit feature for each todoItem

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,15 +1,49 @@
+import {useState} from 'react'
 import './index.css'
 
 const TodoItem = props => {
-  const {todoListItem, onChangeTodo} = props
-  const {title, id} = todoListItem
+  const {todoListItem, onChangeTodo, onEdit, onSave, onToggleCompletion} = props
+  const {title, id, completed, editing} = todoListItem
+  const [newTitle, handleNewTitle] = useState(title)
   const onClickDelete = () => {
     onChangeTodo(id)
   }
 
+  const handleChange = e => {
+    handleNewTitle(e.target.value)
+  }
+
   return (
     <li className="each-item">
-      <p className="item-name">{title}</p>
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => onToggleCompletion(id)}
+      />
+      {editing ? (
+        <input type="text" value={newTitle} onChange={handleChange} />
+      ) : (
+        <p className={completed ? 'item-name completed' : 'item-name'}>
+          {title}
+        </p>
+      )}
+      {editing ? (
+        <button
+          type="button"
+          className="button save-edit-btn"
+          onClick={() => onSave(id, newTitle)}
+        >
+          Save
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="button save-edit-btn"
+          onClick={() => onEdit(id)}
+        >
+          {completed ? 'Completed' : 'Edit'}
+        </button>
+      )}
       <button type="button" onClick={onClickDelete} className="button">
         Delete
       </button>
